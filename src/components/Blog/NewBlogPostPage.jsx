0a5d34b7751621affbbf2d96fb7c3b8c718b592c
pinkadotted <subkmrspm@gmail.com>
2023-06-08 23:39:49 +0800

    added logic for adding new blog post

--- src/components/Blog/NewBlogPostPage.jsx
+++ src/components/Blog/NewBlogPostPage.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 import { Button, Container, Typography, Box } from "@mui/material";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
-import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import DUMMY_POSTS from "../DUMMY_POSTS";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 const NewBlogPostPage = () => {
   const textareaStyles = {
     width: "1000px", // Set the desired width
     height: "400px", // Set the desired height
     resize: "none", // Prevent resizing of the textarea
+  };
+
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputDesc, setInputDesc] = useState("");
+  const [inputText, setInputText] = useState("");
+
+  const submitHandler = () => {
+    console.log(DUMMY_POSTS);
+    const postDetails = {
+      title: inputTitle,
+      description: inputDesc,
+      content: inputText,
+      date: "1/2/2022",
+      id: "dummy",
+    };
+    console.log(postDetails);
+
+    setInputTitle("");
+    setInputDesc("");
+    setInputText("");
+
+    DUMMY_POSTS.push(postDetails);
+    console.log(DUMMY_POSTS);
   };
 
   return (
@@ -24,7 +48,13 @@ const NewBlogPostPage = () => {
           }}
         >
           <Typography variant="h3">Title: </Typography>
-          <TextField fullWidth id="fullWidth" sx={{ marginLeft: "1rem" }} />
+          <TextField
+            fullWidth
+            id="fullWidth"
+            sx={{ marginLeft: "1rem" }}
+            value={inputTitle}
+            onChange={(e) => setInputTitle(e.target.value)}
+          />
         </Box>
         <Box
           sx={{
@@ -35,7 +65,13 @@ const NewBlogPostPage = () => {
           }}
         >
           <Typography variant="h5">Description: </Typography>
-          <TextField fullWidth id="fullWidth" sx={{ marginLeft: "1rem" }} />
+          <TextField
+            fullWidth
+            id="fullWidth"
+            sx={{ marginLeft: "1rem" }}
+            value={inputDesc}
+            onChange={(e) => setInputDesc(e.target.value)}
+          />
         </Box>
         <Box
           sx={{
@@ -52,6 +88,8 @@ const NewBlogPostPage = () => {
             rows={4}
             placeholder="Enter text"
             style={textareaStyles}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
           />
         </Box>
         <Box
@@ -62,7 +100,9 @@ const NewBlogPostPage = () => {
             alignItems: "center",
           }}
         >
-          <Button>Submit</Button>
+          <Link to="/blog">
+            <Button onClick={submitHandler}>Submit</Button>
+          </Link>
         </Box>
       </Container>
     </>
